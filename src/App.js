7f3ef5c555c36185ee7bbad3acda5a0ab591a615
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-03-30 09:11:17 +0200

    readme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactSummernote from "react-summernote";
+import "react-summernote/dist/react-summernote.css"; // import styles
+import "react-summernote/lang/summernote-ko-KR";
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ReactSummernote
+        value="Default value"
+        options={{
+          lang: "ko-KR",
+          height: 350,
+          dialogsInBody: true,
+          toolbar: [
+            ["style", ["style"]],
+            ["font", ["bold", "underline", "clear"]],
+            ["fontname", ["fontname"]],
+            ["para", ["ul", "ol", "paragraph"]],
+            ["table", ["table"]],
+            ["insert", ["link", "picture", "video"]],
+            ["view", ["fullscreen", "codeview"]]
+          ]
+        }}
+        onChange={this.onChange}
+      />
     );
   }
 }
